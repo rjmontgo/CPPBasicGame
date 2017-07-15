b60dc8304fb7a98c97efb3b263012d9dda60c87e
womble <rjmontgo@ncsu.edu>
2017-07-15 12:16:00 -0400

    Made a lot of mistakes, and I think it would be better to restart and try again with a better design now that I understand more about C++.

--- game/game.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GAME
-#define GAME 1
-#include "deck.hpp"
-#include "player.hpp"
-void initiateGame();
-void loadGame(/** String path to file */);
-void playGame(Deck deck, Player player);
-#endif
--- game/interface.hpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef INTERFACE
-#define INTERFACE 1
-int openingMenu();
-
-int playRoundMenu();
-
-int playHand();
-#endif
